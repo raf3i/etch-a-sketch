1b35cad98206368643254399f7fd49a870d830cb
raf3i <fawzirafei@gmail.com>
2022-05-12 01:16:50 +0300

    Implemented slider that resizes the grid

--- etch.js
+++ etch.js
@@ -1,29 +1,50 @@
-const GRIDDIMENSION = 16;
+const slider = document.querySelector('#size');
+let gridDimension = slider.value;
 
 const grid = document.querySelector('.grid');
 
-const row = document.createElement('div');
-row.classList.add('row');
+// Draw initial grid
+drawGrid(gridDimension);
 
-const square = document.createElement('div');
-square.classList.add('square');
+// Draw new grid if user changes size through slider
+slider.addEventListener('change', e => {
+  // console.log(e.target.value);
+  removeAllChildNodes(grid);
+  drawGrid(e.target.value);
+});
 
-// Add squares to row
-let clonedSquare;
-for (let i = 0; i < GRIDDIMENSION; i++) {
-  clonedSquare = square.cloneNode(true);
-  row.appendChild(clonedSquare);
+function drawGrid(dimension) {
+  // Create new div with class row
+  let row = document.createElement('div');
+  row.classList.add('row');
+  
+  // Create new div with class square
+  let square = document.createElement('div');
+  square.classList.add('square');
+
+  // Add squares to row
+  let clonedSquare;
+  for (let i = 0; i < dimension; i++) {
+    clonedSquare = square.cloneNode(true);
+    row.appendChild(clonedSquare);
+  }
+
+  // Add row to grid
+  let clonedRow;
+  for (let i= 0; i < dimension; i++) {
+    clonedRow = row.cloneNode(true);
+    grid.appendChild(clonedRow);
+  }
+
+  // Change square color when hovering
+  const squares = document.querySelectorAll('.square');
+  squares.forEach(square => square.addEventListener('mouseenter', (e) => {
+    e.target.style.backgroundColor = "darkblue";
+  }));
 }
 
-// Add row to grid
-let clonedRow;
-for (let i= 0; i < GRIDDIMENSION; i++) {
-  clonedRow = row.cloneNode(true);
-  grid.appendChild(clonedRow);
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
 }
-
-// Change square color when hovering
-const squares = document.querySelectorAll('.square');
-squares.forEach(square => square.addEventListener('mouseenter', (e) => {
-  e.target.style.backgroundColor = "darkblue";
-}));
