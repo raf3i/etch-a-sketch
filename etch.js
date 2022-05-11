62eabdb3c489342bf50f488b3af52b17c0505e87
raf3i <fawzirafei@gmail.com>
2022-05-11 22:47:39 +0300

    Squares change color upon hovering

--- etch.js
+++ etch.js
@@ -4,17 +4,27 @@ const grid = document.querySelector('.grid');
 
 const row = document.createElement('div');
 row.classList.add('row');
+
 const square = document.createElement('div');
 square.classList.add('square');
 
+// Add squares to row
 let clonedSquare;
 for (let i = 0; i < GRIDDIMENSION; i++) {
   clonedSquare = square.cloneNode(true);
   row.appendChild(clonedSquare);
 }
 
+// Add row to grid
 let clonedRow;
 for (let i= 0; i < GRIDDIMENSION; i++) {
   clonedRow = row.cloneNode(true);
   grid.appendChild(clonedRow);
 }
+
+// Change square color when hovering
+const squares = document.querySelectorAll('.square');
+squares.forEach(square => square.addEventListener('mouseenter', (e) => {
+  console.log(e.target);
+  e.target.style.backgroundColor = "darkblue";
+}));
