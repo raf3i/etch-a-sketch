8aa3416af061e2f787b8b33d709af56149ff3b93
raf3i <fawzirafei@gmail.com>
2022-05-12 21:40:14 +0300

    Page changes color when blue/green buttons are pressed

--- etch.js
+++ etch.js
@@ -1,19 +1,49 @@
 const slider = document.querySelector('.slider');
 let gridDimension = slider.value;
 
-const grid = document.querySelector('.grid');
-
-// Draw initial grid
-drawGrid(gridDimension);
-
 // Draw new grid if user changes size through slider
 slider.addEventListener('change', e => {
   // console.log(e.target.value);
+  gridDimension = e.target.value;
   removeAllChildNodes(grid);
-  drawGrid(e.target.value);
+  drawGrid(gridDimension, mainColor);
 });
 
-function drawGrid(dimension) {
+const header = document.querySelector('.header');
+const displayDimension = document.querySelector('.grid-value');
+const grid = document.querySelector('.grid');
+const controls = document.querySelectorAll('.controls');
+
+// Set default color to green
+let mainColor = 'rgb(0, 187, 0)';
+
+// Change color to blue
+const colorBlue = document.querySelector('button[data-color="blue"]');
+colorBlue.addEventListener('click', e => {
+  if(mainColor == 'blue') return;
+  mainColor = 'blue';
+  removeAllChildNodes(grid);
+  drawGrid(gridDimension, mainColor);
+
+  changeColor(mainColor);
+});
+
+// Change color to green
+const colorGreen = document.querySelector('button[data-color="green"]');
+colorGreen.addEventListener('click', e => {
+  if(mainColor == 'rgb(0, 187, 0)') return;
+  mainColor = 'rgb(0, 187, 0)';
+  removeAllChildNodes(grid);
+  drawGrid(gridDimension, mainColor);
+});
+
+// Draw grid
+drawGrid(gridDimension, mainColor);
+
+function drawGrid(dimension, color) {
+  // Display grid dimension
+  displayDimension.innerHTML = `${dimension} x ${dimension}`;
+
   // Create new div with class row
   let row = document.createElement('div');
   row.classList.add('row');
@@ -21,6 +51,16 @@ function drawGrid(dimension) {
   // Create new div with class square
   let square = document.createElement('div');
   square.classList.add('square');
+
+  // Change page's color
+  let newColor;
+  if (color == 'blue') {
+    newColor = 'lightblue';
+  } else if (color == 'rgb(0, 187, 0)') {
+    newColor = 'rgb(189, 230, 173)';
+  }
+  square.style.backgroundColor = header.style.backgroundColor = newColor;
+  controls.forEach(div => div.style.backgroundColor = newColor);
 
   // Add squares to row
   let clonedSquare;
@@ -39,7 +79,7 @@ function drawGrid(dimension) {
   // Change square color when hovering
   const squares = document.querySelectorAll('.square');
   squares.forEach(square => square.addEventListener('mouseenter', (e) => {
-    e.target.style.backgroundColor = "rgb(0, 187, 0)";
+    e.target.style.backgroundColor = color;
   }));
 }
 
